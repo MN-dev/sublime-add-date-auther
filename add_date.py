1a3f22900749a9b8a9160abcc9bdb600d914b9d4
hunter eyes eg <wadedy2020@gmail.com>
2016-11-18 19:41:21 +0200

    Update add_date.py

--- add_date.py
+++ add_date.py
@@ -1,20 +1,15 @@
 import datetime, getpass
 import sublime, sublime_plugin
 
-class AddDateCommand(sublime_plugin.TextCommand):
-    def run(self, args):
-        content = self.view.substr(sublime.Region(0, self.view.size()))
-        begin = content.find(' * @date')
-        if begin == -1:
-            return
-        end = begin + 19
-        target_region = sublime.Region(begin, end)
-        self.view.sel().clear()
-        self.view.sel().add(target_region)
+class AddDateTimeStampCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") } )
 
-        self.view.run_command("insert_snippet", { "contents": " * @date %s" %  datetime.date.today().strftime("%Y %m %d") } )
+class AddDateStampCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.datetime.now().strftime(" /** \n@auther \n@huntereyes93@gmail.com \n %Y-%m-%d %H:%M:%S \n **/") } )
 
-class DateAndSaveCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.run_command("add_date")
-        self.window.run_command("save")
+class AddTimeStampCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.datetime.now().strftime(" ") } )
+
